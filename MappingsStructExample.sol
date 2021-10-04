7caea45ec46a98a3ecb5507a3d19ac7ac49c9c3a
TrustedDev1325 <olamideliam@gmail.com>
2021-10-05 03:42:14 +0900

    Add withdraw function

--- MappingsStructExample.sol
+++ MappingsStructExample.sol
@@ -13,13 +13,19 @@ contract MappingsStructExample {
         mapping(uint => Payment) payments;
     }
     
-    mapping (address => Balance) public balanceReceived;
+    mapping (address => uint) public balanceReceived;
     
     function getBalance() public view returns(uint){
         return address(this).balance;
     }
     
     function sendMoney() public payable{
-        balanceReceived[msg.sender].totalBalance += msg.value;
+        balanceReceived[msg.sender] += msg.value;
+    }
+    
+    function withdrawAllMoney (address payable _to) public{
+        uint balanceToSend = balanceReceived[msg.sender];
+        balanceReceived[msg.sender]= 0;
+        _to.transfer(balanceToSend);
     }
 }
