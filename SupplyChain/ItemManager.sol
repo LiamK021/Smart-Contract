aa71d54c6035f3cde5e9c3047bd8d88d94157ee7
TrustedDev1325 <olamideliam@gmail.com>
2021-10-06 10:06:53 +0900

    Add Supply Chain

--- SupplyChain/ItemManager.sol
+++ SupplyChain/ItemManager.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8.0;
 
+import "./Ownable.sol";
 import "./Item.sol";
 
-contract ItemManager{
+contract ItemManager is Ownable{
     
     enum SupplyChainSteps {Created, Paid, Delivered}
     
@@ -18,7 +19,7 @@ contract ItemManager{
     
     event SupplyChainStep (uint _itemIndex, uint _step, address _address);
     
-    function createItem (string memory _identifier, uint _priceInWei) public {
+    function createItem (string memory _identifier, uint _priceInWei) public onlyOwner {
         Item item = new Item (this, _priceInWei, index);
         items[index]._item = item;
         //items[index]._priceInWei = _priceInWei;
@@ -37,7 +38,7 @@ contract ItemManager{
         emit SupplyChainStep(_index, uint(items[_index]._step), address(item));
     }
     
-    function triggerDelivery( uint _index) public {
+    function triggerDelivery( uint _index) public onlyOwner {
         require( items[_index]._step == SupplyChainSteps.Paid, "It is further in the supply chain");
         items[_index]._step = SupplyChainSteps.Delivered;
         emit SupplyChainStep(_index, uint(items[_index]._step), address(items[_index]._item));
